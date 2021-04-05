ef8b40a133e8e76149c23c8212bfe3ec2c0516ec
Devin <devin.kinkead@gmail.com>
2021-04-04 21:16:18 -0700

    Closing db connection after use

--- nodeSql.js
+++ nodeSql.js
@@ -41,7 +41,7 @@ exports.dbQuery = function dbQuery(databaseQuery, creds) {
     mysql = require('mysql');
     var dbConfig = creds
     var db = mysql.createConnection(dbConfig); // Recreate the connection, since
-                                                        // the old one cannot be reused
+    // the old one cannot be reused
     return new Promise(data => {
         db.query(databaseQuery, function (error, result) { // change db->connection for your code
             if (error) {
@@ -52,15 +52,17 @@ exports.dbQuery = function dbQuery(databaseQuery, creds) {
                 // console.log(result);
 
                 data(result);
+                
 
             } catch (error) {
                 data({});
+                
                 throw error;
             }
 
         });
+        db.end()
     });
-
 }
     
 
